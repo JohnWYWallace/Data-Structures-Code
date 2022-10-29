600e62d02e7098964d861ff1ae0cf7650d804034
John Wallace <47955658+JohnWYWallace@users.noreply.github.com>
2022-10-29 15:46:08 -0400

    added some comments

--- UserIDSorting.java
+++ UserIDSorting.java
@@ -7,11 +7,11 @@ import java.util.ArrayList;
 
          String pivot = userIDs.get(k);
 
-         int ind = (i-1);
+         int ind = (i - 1);
 
-         for (int j=i; j<k; j++) {
+         for (int j = i; j < k; j++) {
 
-            if (userIDs.get(j).compareTo(pivot)<0) {
+            if (userIDs.get(j).compareTo(pivot) < 0) {
 
                ind++;
 
@@ -23,59 +23,58 @@ import java.util.ArrayList;
 
             }
 
-      }
+      }  
 
-    String temp = userIDs.get(ind+1);
+      String temp = userIDs.get(ind+1);
 
-    userIDs.set(ind+1, userIDs.get(k));
+      userIDs.set(ind+1, userIDs.get(k));
 
-    userIDs.set(k, temp);
+      userIDs.set(k, temp);
 
-    return ind+1;
+      return ind+1;
 
-    }
+    } // end partition()
 
     public static void quicksort(ArrayList<String> UserIDs, int i, int k) {
 
-    if (i < k) {
+      if (i < k) {
 
-      int pi = partition(UserIDs, i, k);
+         int pi = partition(UserIDs, i, k);
 
-      quicksort(UserIDs, i, pi-1);
+         quicksort(UserIDs, i, pi-1);
 
-      quicksort(UserIDs, pi+1, k);
+         quicksort(UserIDs, pi+1, k);
 
-    }
+      }
 
-   }
-    public static void main(String[] args) {
+   }  // end quicksort()
+      
+   public static void main(String[] args) {
 
       Scanner scnr = new Scanner(System.in);
 
-      ArrayList<String> userIDList = new ArrayList<String>();
+      ArrayList<String> userIDList = new ArrayList<String>();  // Instantiate new ArrayList
 
-      String userID;
+      String userID;                                           
 
       userID = scnr.next();
 
       while (!userID.equals("-1")) {
 
-      userIDList.add(userID);
+         userIDList.add(userID);
 
-      userID = scnr.next();
+         userID = scnr.next();
 
-    } 
+      } 
 
-// Initial call to quicksort
+   quicksort(userIDList, 0, userIDList.size() - 1);   // Initial call to quicksort
 
-quicksort(userIDList, 0, userIDList.size() - 1);
-
-   for (int i = 0; i < userIDList.size(); ++i) {
+   for (int i = 0; i < userIDList.size(); ++i) {      // Prints out User IDs after sorting
 
       System.out.println(userIDList.get(i));
 
-      }
+     }
 
-   }
+   }  // end main()
 
-}
+}  // end userIDSorting()
